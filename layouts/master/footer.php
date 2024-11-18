085419b93374e4896c481a4418f2ab047c1e6c34
Zuki <binhminer123@gmail.com>
2024-11-18 22:18:52 +0700

    change so much

--- layouts/master/footer.php
+++ layouts/master/footer.php
@@ -1,54 +1,51 @@
-<footer id="footer" class="bg bg-black text-white footer">
-        <div class="container">
-            <center>
-                <img src="images/logo-GI.png" alt="">
-            </center>
-        </div>
-        <div class="container">
-            <div class="row text-center">
-                <div class="col-lg-5"></div>
-                <div class="col-lg-4" style="margin-left: -50px; margin-top: 5px;">
-                    <nav class="navbar navbar-expand-lg">
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                          <ul class="navbar-nav">
-                            <li class="nav-item">
-                              <a class="nav-link" href="https://genshin.hoyoverse.com/en/company/privacy">Privacy Policy</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="https://genshin.hoyoverse.com/en/company/terms">Terms of Service</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="https://www.hoyoverse.com/about-us?utm_source=genshin&utm_medium=footer">About Us</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="mailto:genshin_cs@hoyoverse.com">Contact Us</a>
-                            </li>
-                          </ul>
-                        </div>
-                    </nav>
+<footer class="text-center text-lg-start bg-light text-muted footer">
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"></section>
+        <section class="">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Selling Zuki glasses
+                        </h6>
+                        <p>
+                            Specializing in selling glasses frames and lenses
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Product
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">Glasses frames</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Lenses</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">Sunglasses</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="#!" class="text-reset">FAQs</a>
+                        </p>
+                        <p>
+                            <a href="#!" class="text-reset">About us</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Contact
+                        </h6>
+                        <p><i class="fas fa-home me-3"> Da Ton, Gia Lam, Ha Noi, Viet Nam</i></p>
+                        <p><i class="fas fa-envelope me-3"> binh63261@gmail.com</i></p>
+                        <p><i class="fas fa-phone me-3">  +84 354497902</i></p>
+                    </div>
                 </div>
-                <div class="col-lg-3"></div>
             </div>
-        </div>
-        <div class="container">
-            <div>
-                <center>
-                    <img style="width: 16%;" src="images/warning-age.png" alt="">
-                </center>
-            </div>
-        </div>
-        <div class="container">
-            <div class="text-center">
-                <p class="text-footer">PlayStation Family Mark," "PS5 logo" and "PS4 logo" are registered trademarks or trademarks of Sony Interactive Entertainment Inc.<br>Epic, Epic Games, Epic Games Store, the Epic Games Store logo, and Epic Online Services are trademarks and/or registered trademarks of Epic Games. All other trademarks are the property of their respective owners.</p>
-            </div>
-        </div>
-        <div class="container">
-            <center>
-                <img style="width: 25%;" src="images/logo-hoyoverse.png" alt="">
-            </center>
-        </div>
-        <div class="container text-center">
-            <p style="font-size: 75%; margin-top: 35px; margin-bottom: 0px; padding-bottom: 25px;">Copyright © COGNOSPHERE. All Rights Reserved.</p>
-        </div>
-        <div></div>
+        </section>
     </footer>
--- layouts/master/header.php
+++ layouts/master/header.php
@@ -1,58 +1,63 @@
-<header id="header" class="p-3 mb-2 bg-primary bg-gradient text-primary-emphasis" style="--bs-bg-opacity: .5;">
-        <div>
-            <div class="row">
-                <div class="col-lg-4">
+<header>
+        <!-- navbar -->
+        <div class="menu">
+            <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+                <div class="container">
                     <div class="row">
-                        <div class="col-lg-4">
-                            <center>
-                                <img style="width: 45%;" src="images/UI_HomeworldLevel_Image_4.png" alt="">
-                            </center>
-                        </div>
-                        <div class="col-lg-8">
+                        <div style="margin-top: -20px; margin-left: 40px;">
                             <nav class="navbar">
-                            <div class="container-fluid">
-                                <form class="d-flex" role="search">
-                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                                    <button id="search" class="btn btn-outline-success" type="submit"><img src="images/UI_Icon_Search.png" alt=""></button>
-                                  </form>
+                                <!-- Logo -->
+                                <a class="navbar-brand" href="#">
+                                    <img src="images/logo.png" alt="Kính mắt Zuki" width="100" height="auto">
+                                </a>
+                                <div style="margin-left: 160px;">
+                                    <a id="item" href="main.html">Home</a>
                                 </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/glasses_frames.html">Glasses frames</a>
+                                </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/Lenses.html">Lenses</a>
+                                </div>
+                                <div style="margin-left: 60px;">
+                                    <a id="item" href="/layouts/products/Sunglasses.html">Sunglasses</a>
+                                </div>
+
+                                <!-- Search bar -->
+                                <div style="margin-left: 180px;">
+                                    <nav class="navbar">
+                                        <form method="post" class="d-flex" role="search">
+                                            <button id="search" class="btn btn-outline-success" type="submit"><img
+                                                    src="images/UI_Icon_Search.png" alt=""></button>
+                                        </form>
+                                    </nav>
+                                </div>
+
+                                <!-- Login/Register -->
+                                <div style="margin-left: 40px;">
+                                    <a id="login" href="#">Login/Register</a>
+                                </div>
+                                <input type="text" id="search_input" placeholder="Search..." style="display: none;">
                             </nav>
                         </div>
+
                     </div>
                 </div>
-                <div class="col-lg-5">
-                    <div id="button-1" class="row">
-                        <div class="col-lg-3">
-                            <button onclick="window.location.href = 'index.html'"" class = "mp btn btn-warning">Trang chủ</button>
-                        </div>
-                        <div class="col-lg-3">
-                            <button onclick="window.location.href = 'item.html'"" class = "mp btn btn-outline-light">Vật phẩm</button>
-                        </div>
-                        <div class="col-lg-3">
-                            <button onclick="window.location.href = 'souvenir.html'"" class = "mp btn btn-outline-light">Lưu niệm</button>
-                        </div>
-                        <div class="col-lg-3">
-                            <button onclick="window.location.href = 'account.html'"" class = "mp btn btn-outline-light">Tài khoản</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="row">
-                        <div class="col-lg-1"></div>
-                        <div class="col-lg-2">
-                            <button id="shop" onclick="window.location.href = 'shop.html'" class="btn btn-secondary">
-                                <center>
-                                    <img style="width: 70%;" src="images/UI_Icon_Shop.png" alt="">
-                                </center>
-                            </button>
-                        </div>
-                        <div class="col-lg-1"></div>
-                        <div id="button-2" class="col-lg-8">
-                            <button onclick="window.location.href = 'index.html'" class = "main-page btn btn-info text-end" style="margin-right: 5%">Đăng nhập</button>
-                            <button onclick="window.location.href = 'index.html'" class = "main-page btn btn-success text-end">Đăng kí</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <!-- js for search tab -->
+                <script>
+                    const searchBtn = document.getElementById('search');
+                    const searchInput = document.getElementById('search_input');
+
+                    searchBtn.addEventListener('mouseover',
+                        () => {
+                            searchInput.style.display = 'inline';
+                            searchInput.focus();
+                        });
+
+                    searchBtn.addEventListener('mouseout', () => {
+                        searchInput.style.display = 'none';
+                    });
+                </script>
+            </nav>
         </div>
     </header>
